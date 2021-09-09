9799ec1caf6bffec87f206cf520df7c885226250
Graham Russell <graham@ham1.co.uk>
2021-09-09 17:20:23 +0100

    Fix SeleniumDownloadPage HeaderComponent link selector

--- src/test/java/com/frameworkium/integration/seleniumhq/pages/SeleniumDownloadPage.java
+++ src/test/java/com/frameworkium/integration/seleniumhq/pages/SeleniumDownloadPage.java
@@ -14,7 +14,7 @@ public class SeleniumDownloadPage extends BasePage<SeleniumDownloadPage> {
     private HeaderComponent header;
 
     @Visible
-    @FindBy(css = "main > div:nth-child(3)  p:nth-child(1) > a")
+    @FindBy(css = "main > div:nth-child(3) p:nth-child(1) > a")
     private Link latestDownloadLink;
 
     public static SeleniumDownloadPage open() {
