6016917b7a157dc41c003f865799b568a17db331
Graham Russell <graham@ham1.co.uk>
2024-03-08 16:16:09 +0000

    Fix spotless

--- src/test/java/com/frameworkium/integration/frameworkium/tests/ReInitDriverTest.java
+++ src/test/java/com/frameworkium/integration/frameworkium/tests/ReInitDriverTest.java
@@ -2,6 +2,7 @@ package com.frameworkium.integration.frameworkium.tests;
 
 import com.frameworkium.integration.seleniumhq.pages.HomePage;
 import com.frameworkium.lite.ui.tests.BaseUITest;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.RepeatedTest;
 import org.openqa.selenium.remote.UnreachableBrowserException;
@@ -10,7 +11,6 @@ import org.openqa.selenium.remote.UnreachableBrowserException;
 public class ReInitDriverTest extends BaseUITest {
 
     private static boolean retry = true;
-
 
     @RepeatedTest(value = 2, name = "Reinitialise driver test")
     public void browser_is_re_initialised_if_test_is_restarted() {
